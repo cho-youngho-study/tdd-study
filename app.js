4b0e8a65f1cfcf42d31fa0ee8aeb753e23ad1abb
ChoYoungHo <choyoungho95@gmail.com>
2021-03-17 22:54:43 +0900

    테스트 환경 개선(test시 logger 나타나지 않게 하기)

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 var indexRouter = require('./routes/users');
 
 var app = express();
 
-app.use(logger('dev'));
+if (process.env.NODE_ENV !== 'test') {
+    app.use(logger('dev'));
+}
 app.use(express.json());
 app.use(express.urlencoded({
     extended: false
